dd90105b59e79156b1a851b14a125dda6d7b553e
TanushriKatyarmal <tanushrik304@gmail.com>
2023-12-20 13:57:12 +0530

    file added

---
+++ src/Data.js
@@ -0,0 +1,33 @@
+const Data=[
+    {
+        'name':"Shahrukh Khan",
+        'age' : 57,
+        'image': "https://in.bmscdn.com/iedb/artist/images/website/poster/large/shah-rukh-khan-2092-12-09-2017-02-10-43.jpg"
+    }
+    ,
+    {
+        'name':"Akshay Kumar",
+        'age' : 45,
+        'image': "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQKW-E2ANTMDwWeGQxb8TdD8cVryY2aMMbCRqYqy-x3JkW-oz0O"
+    }
+    ,
+    {
+        'name':"Rohit Sharma",
+        'age' : 38,
+        'image': "https://images.news18.com/ibnlive/uploads/2023/11/copy-of-sports-2023-11-12t223755.572-2023-11-e1fffba21b50748444647dc8888221c1-3x2.jpg?impolicy=website&width=360&height=240"
+    }
+    ,
+    {
+        'name':"Virat Kohli",
+        'age' : 37,
+        'image': "https://sambadenglish.com/wp-content/uploads/2022/11/Virat-Kohli-1.jpg"
+    }
+    ,
+    {
+        'name':"Rashmika Mandhana",
+        'age' : 25,
+        'image': "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-nKxdYBGWWXHmSvSvMKj9fnuPQm2CvIvxEg&usqp=CAU"
+    }
+    
+]
+export default Data
